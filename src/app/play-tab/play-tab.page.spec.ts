f1c407d677f2eff77260d0d3690c761916584894
Ramakrishnan Raghu <raguramrec@gmail.com>
2020-02-04 00:05:37 -0500

    saving changes

--- src/app/play-tab/play-tab.page.spec.ts
+++ src/app/play-tab/play-tab.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { Tab2Page } from './tab2.page';
+import { PlayTabPage } from './play-tab.page';
 
-describe('Tab2Page', () => {
-  let component: Tab2Page;
-  let fixture: ComponentFixture<Tab2Page>;
+describe('PlayTabPage', () => {
+  let component: PlayTabPage;
+  let fixture: ComponentFixture<PlayTabPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab2Page],
+      declarations: [PlayTabPage],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab2Page);
+    fixture = TestBed.createComponent(PlayTabPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
